01685a1289979292c3c8f93575c20d1866c013a6
Infinital-X <bosstoxa2000@mail.ru>
2024-03-02 18:32:51 +0200

    hw7

---
+++ app/src/main/java/com/example/hw7/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.hw7;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String APP_LAUNCH_COUNTER = "appLaunchCount";
+    private static final String PREFERENCES_NAME = "myPreferences";
+    private int chosenRating;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent serviceIntent = new Intent(this, BackgroundService.class);
+        startService(serviceIntent);
+    }
+}
